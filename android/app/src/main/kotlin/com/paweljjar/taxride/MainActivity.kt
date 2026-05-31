55a2734e9085d5a2d870f21b5c986c04e2c256d3
Paweł Jaroszewicz <gaweljar0@outlook.com>
2026-05-31 14:33:54 +0200

    Initial project setup and implementation of the main application structure.
    - Initialize Flutter project with Android, iOS, and Windows platform support.
    - Implement `HomePage` in `lib/Home.dart` featuring a `TabBarView` and `BottomNavigationBar` for Taxes, Invoices, and Incomes sections.
    - Configure `MyApp` in `lib/main.dart` with Material 3 and custom theme colors.
    - Set up Android build configurations using Kotlin DSL and update application metadata.
    - Configure iOS Runner project, including app icons, launch screens, and workspace settings.
    - Initialize Windows runner with CMake build scripts and Win32 window management logic.
    - Add base widget tests and standard project configuration files (`pubspec.yaml`, `analysis_options.yaml`, `.gitignore`).

---
+++ android/app/src/main/kotlin/com/paweljjar/taxride/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.paweljjar.taxride
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(
+            child: Text('Taxes'),
+          ),
+          Center(
+            child: Text('Invoices'),
+          ),
+          Center(
+            child: Text('Incomes'),
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Theme.of(context).primaryColor,
+        child: TabBar(
+          controller: _tabController,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white70,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorPadding: EdgeInsets.all(5.0),
+          indicatorColor: Colors.white,
+          tabs: const [
+            Tab(icon: Icon(Icons.money_off), text: 'Podatki'),
+            Tab(icon: Icon(Icons.text_snippet), text: 'Faktury'),
+            Tab(icon: Icon(Icons.attach_money_rounded), text: 'Przychód'),
+          ],
+        )
+      )
+    );
+  }
+}
